71b177605f93011d0b5d3c0c1d7d9253d5c6e758
user <user@DESKTOP-P8B9C8T>
2018-03-28 18:18:17 +0900

    dskim

--- src/main/java/com/newlecture/controller/member/NoteController.java
+++ src/main/java/com/newlecture/controller/member/NoteController.java
@@ -1,9 +1,19 @@
 package com.newlecture.controller.member;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.newlecture.entity.Note;
 import com.newlecture.service.member.NoteService;
@@ -45,10 +55,49 @@ public class NoteController {
 	// 폼에서 전송된 데이터의 name과 매서드의 파라미터 이름,자료형이 같다면 자동으로 파라미터로 전달됨
 	// 매서드의 파라미터를 구조화된 데이터로 설정하면 전달된 폼 데이터의 이름을 파라미터의 셋터함수와 비교하여 변수명이 같을경우 객체화하여 전달
 	@RequestMapping(value="reg", method=RequestMethod.POST)
-	public String reg(Note note) {
+	
+	public String reg(Note note, MultipartFile file, HttpServletRequest request) {
+		
+		String memberId = "newlec";
+		ServletContext ctx = request.getServletContext();
+		String path = ctx.getRealPath("/resources/note/" + memberId);
+		
+	    File f = new File(path);
+	    if(!f.exists())
+	    	f.mkdirs();
+	    
+		if(!file.isEmpty()) {
+			try {
+				String fileName = file.getOriginalFilename();
+				
+				InputStream fis = file.getInputStream();
+				
+				FileOutputStream fos = new FileOutputStream(path + File.separator + fileName);
+				
+				byte[] buf = new byte[1024];
+				int size = 0;
+				while((size = fis.read(buf,0,1024)) != -1) {
+					fos.write(buf,0,size);
+				}
+				fis.close();
+				fos.close();
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 		service.insertNote(note);
 		
 		return "redirect:list";	// list페이지로 리다이렉트 (현재 url기준으로 리다이렉트)
 	}
+	
+	@RequestMapping("data")
+	@ResponseBody	// 매서드의 리턴값이 뷰가 아닌 데이터임을 정의하는 어노테이션
+	public String data() {
+		
+		return "안녕하세요";	//뷰가 아니라는 어노테이션을 달아줄수 있음
+	}
+	
 }
--- src/main/webapp/WEB-INF/views/member/note/reg.jsp
+++ src/main/webapp/WEB-INF/views/member/note/reg.jsp
@@ -4,17 +4,25 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=1">
+ <!-- initial-scale=1 <== 장치의 가로길이를 1밀도로 맞춰라 -->
+ <!-- minimum-scale=1 <== 최소 크기를 1밀도 이하로 지정 안돼도록 설정 -->
+ <!-- user-scalable=1 <== 줌기능 안되도록 설정 -->
 <title>노트 등록 폼</title>
 </head>
 <body>
 	<main>
 		<section>
 			<h1>노트 등록 폼</h1>
-			<form method="post">
+			<form method="post" enctype="multipart/form-data">
 				<table>
 					<tr>
 						<th>제목</th>
 						<td><input type="text" name="title" value=""/></td>
+					</tr>
+					<tr>
+						<th>첨부파일</th>
+						<td><input type="file" name="file"/></td>
 					</tr>
 					<tr>
 						<th>내용</th>
@@ -31,3 +39,20 @@
 	</main>
 </body>
 </html>
+
+<script>
+	window.addEventListener("load",function(){
+		var submitButton = document.querySelector("input[value='등록']");
+		submitButton.onclick = function(e){
+			
+			var request = new XMLHttpRequest();
+			request.onload = function(evt){
+				alert(request.responseText);
+			}
+			request.open("GET", "data");
+			request.send();
+
+			//e.preventDefault();
+		}
+	});
+</script>
